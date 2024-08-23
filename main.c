cef4c76836ab5ed62a7969a8d3f6d0f4135ba79b
Karol Moroz <karol@moroz.dev>
2024-08-23 18:31:46 +0800

    1 * 2 + 3

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 #include "chunk.h"
-#include "debug.h"
 #include "vm.h"
 
 int main(int argc, const char *argv[]) {
@@ -8,26 +7,25 @@ int main(int argc, const char *argv[]) {
   Chunk chunk;
   initChunk(&chunk);
 
-  int constant = addConstant(&chunk, 1.2);
+  int constant = addConstant(&chunk, 1);
   writeChunk(&chunk, OP_CONSTANT, 123);
   writeChunk(&chunk, constant, 123);
 
-  constant = addConstant(&chunk, 3.4);
+  constant = addConstant(&chunk, 2);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  writeChunk(&chunk, OP_MULTIPLY, 123);
+
+  constant = addConstant(&chunk, 3);
   writeChunk(&chunk, OP_CONSTANT, 123);
   writeChunk(&chunk, constant, 123);
 
   writeChunk(&chunk, OP_ADD, 123);
 
-  constant = addConstant(&chunk, 5.6);
-  writeChunk(&chunk, OP_CONSTANT, 123);
-  writeChunk(&chunk, constant, 123);
-
-  writeChunk(&chunk, OP_DIVIDE, 123);
-  writeChunk(&chunk, OP_NEGATE, 123);
-
   writeChunk(&chunk, OP_RETURN, 123);
 
-  disassembleChunk(&chunk, "test chunk");
+  // disassembleChunk(&chunk, "test chunk");
   interpret(&chunk);
   freeVM();
   freeChunk(&chunk);
