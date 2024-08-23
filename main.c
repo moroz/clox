49784020e5ee39240586e36ad7acbb25169d9548
Karol Moroz <karol@moroz.dev>
2024-08-23 18:32:22 +0800

    1 + 2 * 3

--- main.c
+++ main.c
@@ -7,17 +7,17 @@ int main(int argc, const char *argv[]) {
   Chunk chunk;
   initChunk(&chunk);
 
-  int constant = addConstant(&chunk, 1);
+  int constant = addConstant(&chunk, 2);
   writeChunk(&chunk, OP_CONSTANT, 123);
   writeChunk(&chunk, constant, 123);
 
-  constant = addConstant(&chunk, 2);
+  constant = addConstant(&chunk, 3);
   writeChunk(&chunk, OP_CONSTANT, 123);
   writeChunk(&chunk, constant, 123);
 
   writeChunk(&chunk, OP_MULTIPLY, 123);
 
-  constant = addConstant(&chunk, 3);
+  constant = addConstant(&chunk, 1);
   writeChunk(&chunk, OP_CONSTANT, 123);
   writeChunk(&chunk, constant, 123);
 
