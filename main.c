cddae1459df836512871cc539206bb862288a6f4
Karol Moroz <karol@moroz.dev>
2024-08-23 18:33:09 +0800

    3 - 2 - 1

--- main.c
+++ main.c
@@ -7,21 +7,21 @@ int main(int argc, const char *argv[]) {
   Chunk chunk;
   initChunk(&chunk);
 
-  int constant = addConstant(&chunk, 2);
+  int constant = addConstant(&chunk, 3);
   writeChunk(&chunk, OP_CONSTANT, 123);
   writeChunk(&chunk, constant, 123);
 
-  constant = addConstant(&chunk, 3);
+  constant = addConstant(&chunk, 2);
   writeChunk(&chunk, OP_CONSTANT, 123);
   writeChunk(&chunk, constant, 123);
 
-  writeChunk(&chunk, OP_MULTIPLY, 123);
+  writeChunk(&chunk, OP_SUBTRACT, 123);
 
   constant = addConstant(&chunk, 1);
   writeChunk(&chunk, OP_CONSTANT, 123);
   writeChunk(&chunk, constant, 123);
 
-  writeChunk(&chunk, OP_ADD, 123);
+  writeChunk(&chunk, OP_SUBTRACT, 123);
 
   writeChunk(&chunk, OP_RETURN, 123);
 
